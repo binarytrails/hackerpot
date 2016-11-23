ad199d0b28d70bac0f5247e4d8a06784e85e3b64
Seva Ivanov <seva@tumahn.net>
2016-11-23 01:25:03 +0000

    hackerpot: add rpi_ws281x strandtest animations

--- HoneyPy/HackerPot.py
+++ HoneyPy/HackerPot.py
@@ -13,8 +13,17 @@ class HackerPot:
 
     hackers = dict()
 
+    strip = Adafruit_NeoPixel(
+        LED_COUNT,
+        LED_PIN,
+        LED_FREQ_HZ,
+        LED_DMA,
+        LED_INVERT,
+        LED_BRIGHTNESS
+    )
+
     def __init__(self):
-        pass
+        self.strip.begin()
 
     def connection(self, peer):
         print('Connection Detected from %s' % peer.host)
@@ -24,6 +33,8 @@ class HackerPot:
         else:
             self.hackers[peer.host]['connections'] += 1
 
+        self.run_all_animations()
+
     def intrusion(self, peer):
         print('Intrusion Detected from %s' % peer.host)
 
@@ -31,4 +42,75 @@ class HackerPot:
             self.hackers[peer.host] = {'connections': 0, 'intrusions': 1}
         else:
             self.hackers[peer.host]['intrusions'] += 1
+
+    def colorWipe(self, color, wait_ms=50):
+        """Wipe color across display a pixel at a time."""
+
+        for i in range(self.strip.numPixels()):
+            self.strip.setPixelColor(i, color)
+            self.strip.show()
+            time.sleep(wait_ms/1000.0)
+
+    def theaterChase(self, color, wait_ms=50, iterations=10):
+            """Movie theater light style chaser animation."""
+            for j in range(iterations):
+                for q in range(3):
+                    for i in range(0, self.strip.numPixels(), 3):
+                        self.strip.setPixelColor(i+q, color)
+                    self.strip.show()
+                    time.sleep(wait_ms/1000.0)
+                    for i in range(0, self.strip.numPixels(), 3):
+                        self.strip.setPixelColor(i+q, 0)
+
+    def wheel(self, pos):
+        """Generate rainbow colors across 0-255 positions."""
+        if pos < 85:
+            return Color(pos * 3, 255 - pos * 3, 0)
+        elif pos < 170:
+            pos -= 85
+            return Color(255 - pos * 3, 0, pos * 3)
+        else:
+            pos -= 170
+            return Color(0, pos * 3, 255 - pos * 3)
+
+    def rainbow(self, wait_ms=20, iterations=1):
+        """Draw rainbow that fades across all pixels at once."""
+        for j in range(256*iterations):
+            for i in range(self.strip.numPixels()):
+                self.strip.setPixelColor(i, wheel((i+j) & 255))
+            self.strip.show()
+            time.sleep(wait_ms/1000.0)
+
+    def rainbowCycle(self, wait_ms=20, iterations=5):
+        """Draw rainbow that uniformly distributes itself across all pixels."""
+        for j in range(256*iterations):
+            for i in range(self.strip.numPixels()):
+                self.strip.setPixelColor(i, wheel(((i * 256 / self.strip.numPixels()) + j) & 255))
+            self.strip.show()
+                time.sleep(wait_ms/1000.0)
+
+    def theaterChaseRainbow(self, wait_ms=50):
+        """Rainbow movie theater light style chaser animation."""
+        for j in range(256):
+            for q in range(3):
+                for i in range(0, self.strip.numPixels(), 3):
+                    self.strip.setPixelColor(i+q, wheel((i+j) % 255))
+                self.strip.show()
+                time.sleep(wait_ms/1000.0)
+                for i in range(0, self.strip.numPixels(), 3):
+                    self.strip.setPixelColor(i+q, 0)
+
+    def run_all_animations(self):
+        # Color wipe animations.
+        self.colorWipe(Color(255, 0, 0))  # Red wipe
+        self.colorWipe(Color(0, 255, 0))  # Blue wipe
+        self.colorWipe(Color(0, 0, 255))  # Green wipe
+        # Theater chase animations.
+        self.theaterChase(Color(127, 127, 127))  # White theater chase
+        self.theaterChase(Color(127,   0,   0))  # Red theater chase
+        self.theaterChase(Color(  0,   0, 127))  # Blue theater chase
+        # Rainbow animations.
+        self.rainbow()
+        self.rainbowCycle()
+        self.theaterChaseRainbow()
 
