4aa01f8b1159d3b07dde5fd83a875a8ed1dd2b1e
Seva Ivanov <seva@tumahn.net>
2016-12-07 23:38:43 +0000

    extend stem animations to pot

--- HoneyPy/HackerPotAnimationsTests.py
+++ HoneyPy/HackerPotAnimationsTests.py
@@ -50,6 +50,11 @@ class HackerPotAnimations:
         for led in range(self.MAX_LEDS):
             self.SERIAL.write(bytearray([led] + rgb))
             time.sleep(delay)
+    
+    def colorize_base(self, start_led, end_led, rgb, delay):
+        for led in range(start_led, end_led):
+            self.SERIAL.write(bytearray([led] + rgb))
+            time.sleep(delay)
 
     def animate_attack(self, key, rgb, new_rgb, delay):
 
@@ -85,18 +90,24 @@ class HackerPotAnimations:
             self.SERIAL.write(bytearray(splixel))
             time.sleep(delay)
 
+delay = 0.01
+
 anims = HackerPotAnimations(ser, 60)
 
-#anims.colorize([255, 0, 0], 0.01)
-anims.recolorize(0.01)
+#anims.colorize([255, 0, 0], delay)
+anims.recolorize(delay)
 
 # everything
 while 1:
 
     # connection (evolves into blue)
-    anims.animate_attack('one', [0, 0, 255], [0, -255, 255], 0.01)
+    anims.animate_attack('one', [0, 0, 255], [0, -255, 255], delay)
+    anims.colorize_base(0, 6, anims.one['rgb'], delay)
     
     # intrustion (evolves into red)
-    anims.animate_attack('three', [255, 0, 0], [255, -255, 0], 0.01)
+    anims.animate_attack('two', [255, 0, 0], [255, -255, 0], delay)
+    anims.colorize_base(9, 14, anims.two['rgb'], delay)
 
-    # TODO third as ddos
+    # dos (evolves into purple)
+    anims.animate_attack('three', [80, 0, 80], [80, -255, 80], delay)
+    anims.colorize_base(6, 9, anims.three['rgb'], delay)
